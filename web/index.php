c054eeacdd9a1ddc6d3def7afc98ddbb4f156d38
jhonsocrex <jhon@socrexcom>
2014-11-05 18:47:00 -0500

    how are you fix
    how are you fix

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello, how are you?’;
+  return 'Hello how are you';
 });
 
 $app->run();
